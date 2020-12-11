683ee3203267b9788a7c82be5666e56f6778a1d7
Rodolfo Heredia <a2019115883@estudiantes.upsa.edu.bo>
2020-12-11 18:22:36 -0400

    Practicaa

---
+++ PracFinal/PracFinal/Controllers/OperacionesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracFinal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperacionesController : ControllerBase {
+        [HttpGet]
+        public string Add(int numero)
+        {
+            if (numero == 0)
+                return "Rodolfo";
+            else if (numero < 0)
+                return "Error";
+            else
+                return $"{numero}";
+
+        }
+    
+    }
+}
